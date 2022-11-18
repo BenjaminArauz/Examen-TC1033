323e808f9cc8ac7104ad60731e220274c9316548
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-11-17 23:52:08 -0600

    Clase "Equipo"

--- Equipo.h
+++ Equipo.h
@@ -16,40 +16,34 @@ class Equipo {
             tam = tamanio; 
         }
         //Metodos
-        string getNombreEquipo();
-        string getEntrenador();
-        int getPuntosAnotados();
-        void setNombreEquipo(string);    
-        void setEntrenador(string);    
-        void setPuntosAnotados(int); 
+        Jugador getInfoJugador(){
+            return info_jugador; 
+        }
+        string getNombreEquipo(){
+            return nombre_equipo;
+        }
+        string getEntrenador(){
+            return entrenador;
+        }
+        void setNombreEquipo(string nuevo_nombre_equipo){
+            nombre_equipo = nuevo_nombre_equipo;
+        }   
+        void setEntrenador(string nuevo_entrenador){
+            entrenador = nuevo_entrenador;
+        }  
+        void setInfoJugador(Jugador nuevo_info_jugador){
+            info_jugador = nuevo_info_jugador;
+        }
         //Otros metodos   
-        int calcularPuntos();
+        int calcularPuntos(){
+            int suma = 0, indice_nuevo;
+            for (int i=0;i<sizeof(tam);i++){
+                indice_nuevo = i;
+                info_jugador.setIndiceJugador(indice_nuevo);
+                suma += info_jugador.getPuntosAnotados();
+            }
+            return suma;
+        }
 };
-string Equipo::getNombreEquipo(){
-    return nombre_equipo;
-}
-string Equipo::getEntrenador(){
-    return entrenador;
-}
-//int Equipo::getPuntosAnotados(){
-//    return puntos_anotados;
-//}
-void Equipo::setNombreEquipo(string nombre_equipo){
-    this->nombre_equipo = nombre_equipo;
-}
-void Equipo::setEntrenador(string nuevo_entrenador){
-    entrenador = nuevo_entrenador;
-}
-void Equipo::setPuntosAnotados(int puntos){
-    //puntos_anotados = puntos;
-}
-int Equipo::calcularPuntos(){
-    int suma = 0, indice_nuevo;
-    for (int i=0;i<sizeof(tam);i++){
-        indice_nuevo = i;
-        info_jugador.setIndiceJugador(indice_nuevo);
-        suma += info_jugador.getPuntosAnotados();
-    }
-    return suma;
-}
+
 #endif
