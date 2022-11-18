202fc4225368b1619a71123b39697f7f5aef104b
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-11-17 23:43:39 -0600

    Clase "Jugador"

--- Jugador.h
+++ Jugador.h
@@ -1,67 +1,133 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
 #include <string>
+#include <vector>
 using namespace std;
 
 class Jugador {
     private:
         //Atributos
-        string nombre;
-        int tam, numero, id, minutos_jugados, puntos_anotados, sueldo_base;
+        int indice_jugador;
+        vector<int> numero;
+        vector<int> puntos_anotados;
+        vector<int> minutos_jugados;
+        vector<int> id;
+        vector<int> sueldo_base;
+        vector<string> nombre;
     public:
         //Constructor
-        Jugador (){}
-        Jugador(int tamanio){
-            tam = tamanio;
-            //numero = new int[tam-1];
-        }
+        Jugador(){}
         //Métodos
-        string getNombre();
-        int getNumero();
-        int getId();
-        int getMinutosJugados();
-        int getPuntosAnotados();
-        int getSueldoBase();
-        void setNombre(string new_nombre);
-        void setNumero(int new_numero);
-        void setId(int new_id);
-        void setMinutosJugados(int new_minutos_jugados);
-        void setPuntosAnotados(int new_puntos_anotados);
-        void setSueldoBase(int new_sueldo);
-
-
+        string getNombre(){
+            return nombre[indice_jugador];
+        }
+        int getNumero(){
+            return numero[indice_jugador];
+        }
+        int getId(){
+            return id[indice_jugador];
+        }
+        int getMinutosJugados(){
+            return minutos_jugados[indice_jugador];
+        }
+        int getPuntosAnotados(){
+            return puntos_anotados[indice_jugador];
+        }
+        int getSueldoBase(){
+            return sueldo_base[indice_jugador];
+        }
+        int getIndiceJugador(){
+            return indice_jugador;
+        }
+        void setNombre(string nuevo_nombre){
+            nombre.push_back(nuevo_nombre);
+        }
+        void setNumero(int nuevo_numero){
+            numero.push_back(nuevo_numero);
+        }
+        void setId(int nuevo_id){
+            id.push_back(nuevo_id);
+        }
+        void setMinutosJugados(int nuevo_minutos_jugados){
+            minutos_jugados.push_back(nuevo_minutos_jugados);
+        }
+        void setPuntosAnotados(int nuevo_puntos_anotados){
+            puntos_anotados.push_back(nuevo_puntos_anotados);
+        }
+        void setSueldoBase(int nuevo_sueldo){
+            sueldo_base.push_back(nuevo_sueldo);
+        }
+        void setIndiceJugador(int nuevo_indice){
+            indice_jugador = nuevo_indice;
+        }
+        //Metodos adicionales
+        void agregarJugadores(){
+            string nombre_agregar;
+            int numero_agregar, puntos_anotados_agregar, minutos_jugados_agregar, id_agregar, sueldo_base_agregar, opcion_minutos_jugados;
+            cout << "Nombre: ";
+            cin >> nombre_agregar;
+            nombre.push_back(nombre_agregar);
+            cout << "Número de camiseta: ";
+            cin >> numero_agregar;
+            numero.push_back(numero_agregar);
+            cout << "Nómina";
+            cin >> id_agregar;
+            id.push_back(id_agregar);
+            cout << "¿El jugador  " << nombre_agregar << "tiene minutos jugados? 1=Si, 2=No: ";
+            cin >> opcion_minutos_jugados;
+            if (opcion_minutos_jugados == 2){
+                minutos_jugados.push_back(0);
+                puntos_anotados.push_back(0);
+            } else {
+                cout << "Minutos jugados: ";
+                cin >> minutos_jugados_agregar;
+                minutos_jugados.push_back(minutos_jugados_agregar);
+                cout << "Puntos anotados: ";
+                cin >> puntos_anotados_agregar;
+                puntos_anotados.push_back(puntos_anotados_agregar);
+            }
+            cout << "Sueldo: ";
+            cin >> sueldo_base_agregar;
+            sueldo_base.push_back(sueldo_base_agregar);
+        }
+        void imprimirInformacion(){
+            cout << "Nombre: " << nombre[indice_jugador];
+            cout << "Nómina: " << id[indice_jugador];
+            cout << "Número: " << numero[indice_jugador];
+            cout << "Minutos jugados: " << minutos_jugados[indice_jugador];
+            cout << "Puntos_anotados: " << puntos_anotados[indice_jugador];
+            cout << "Sueldo base: " << sueldo_base[indice_jugador];
+        }
+        void buscarJugador(string nombre_buscar){
+            int count = 0;
+            for (int i=0;i<sizeof(nombre);i++){
+                if (nombre[i] == nombre_buscar){
+                    indice_jugador = count;
+                }
+                count ++;
+            }
+            if (indice_jugador == 0){
+                cout << "Jugador no encontrado";
+            }
+        }
+        void mayorEncestador(){
+            int mayor_encestador = puntos_anotados[0];
+            for (int i=0;i<sizeof(puntos_anotados);i++){
+                if (puntos_anotados[i] > mayor_encestador){
+                    mayor_encestador = puntos_anotados[i];
+                    indice_jugador = i;
+                }
+            }
+        }
+        void masMinutosCancha(){
+            int mas_minutos = minutos_jugados[0];
+            for (int i=0;i<sizeof(minutos_jugados);i++){
+                if (minutos_jugados[i] > mas_minutos){
+                    mas_minutos = minutos_jugados[i];
+                    indice_jugador = i;
+                }
+            }
+        }
 } ;
-string Jugador::getNombre(){
-    return nombre;
-}
-int Jugador::getNumero(){
-    return numero;
-}
-int Jugador::getId(){
-    return id;
-}
-int Jugador::getMinutosJugados(){
-    return minutos_jugados;
-}
-int Jugador::getPuntosAnotados(){
-    return puntos_anotados;
-}
-int Jugador::getSueldoBase(){
-    return sueldo_base;
-}
-void Jugador::setNombre(string new_nombre){
-    nombre = new_nombre;
-}
-void Jugador::setNumero(int new_numero){
-    numero = new_numero;
-}
-void Jugador::setId(int new_id){
-    id = new_id;
-}
-void Jugador::setMinutosJugados(int new_minutos_jugados){
-    minutos_jugados = new_minutos_jugados;
-}
-void Jugador::setPuntosAnotados(int new_puntos_anotados){
-    puntos_anotados = new_puntos_anotados;
-}
-void Jugador::setSueldoBase(int new_sueldo){
-    sueldo_base = new_sueldo;
-}
+
+#endif
