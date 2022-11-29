5160abb3af5ea1af31249d500f5f86097583a549
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-11-28 23:21:37 -0600

    Add files via upload

--- A00836936_main_examen.cpp
+++ A00836936_main_examen.cpp
@@ -1,71 +1,111 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#include "Jugador.h"
-#include "AtrasCancha.h"
 #include "Equipo.h"
-void menu(){
+#include "Jugador.h"
+#include "Partidos.h"
+#include "Armador.h"
+#include "Alero.h"
+#include "Poste.h"
+
+void menu_administrar() {
     cout << "1. Consultar \n";
     cout << "2. Mayor encestador \n";
-    cout << "3. Sueldo del jugador \n";
+    cout << "3. Cambiar información \n";
     cout << "4. Agregar jugador \n";
     cout << "5. Jugador con mas minutos en cancha \n";
-    cout << "6. Nombre del equipo \n";
-    cout << "7. Puntos anotados del equipo \n";  
-    cout << "8. Entrenador \n";  
-    cout << "9. Tipo de balón \n";
-    cout << "10. Tipo de aro \n";
+    cout << "6. Cambiar nombre del equipo \n";
+    cout << "7. Puntos anotados del equipo \n";
+    cout << "8. Eliminar jugadores \n";
     cout << "0. Salir \n";
 }
-
-int main(){
-    string nombre_buscar, nombre_equipo_nuevo, nombre_entrenador;
-    int tam, opcion;
-    cout << "Cantidad de jugadores a agregar: ";
-    cin >> tam;
-    Jugador jugador_prueba;
-    Equipo equipo(tam);
-    for (int i=0;i<tam;i++){
-        jugador_prueba.agregarJugadores();
+void posicionJugador(){
+    int opcion_posicion;
+    int robos_hechos;
+    int buenas_jugadas;
+    int rebotes;
+    cout << "¿ Qué tipo de jugador es? ";
+    cout << "1. Armador";
+    cout << "2. Alero";
+    cout << "3. Poste";
+    cin >> opcion_posicion;
+    if (opcion_posicion == 1) {
+        cout << "Robos hechos: ";
+        cin >> robos_hechos;
+        Armador armador(robos_hechos);
+    } else if (opcion_posicion == 2) {
+        cout << "Buenas jugadas: ";
+        cin >> buenas_jugadas;
+        Alero alero(buenas_jugadas);
+    } else {
+        cout << "Rebotes hechos: ";
+        cin >> rebotes;
+        Poste poste(rebotes);
     }
-    menu();
-    cin >> opcion;
-    if (opcion == 1){
-        cout << "Nombre: ";
-        cin >> nombre_buscar;
-        jugador_prueba.buscarJugador(nombre_buscar);
-        jugador_prueba.imprimirInformacion();
-    } else if (opcion == 2){
-        jugador_prueba.mayorEncestador();
-        cout << "Mayor encestador: " << jugador_prueba.getNombre() << " con " << jugador_prueba.getPuntosAnotados() << " puntos";
-    } else if (opcion == 3){
-        cout << "Nombre: ";
-        cin >> nombre_buscar;
-        jugador_prueba.buscarJugador(nombre_buscar);
-        cout << "El jugador " << jugador_prueba.getNombre() << " el sueldo es " << jugador_prueba.getSueldoBase();
-    } else if (opcion == 4){
-        jugador_prueba.agregarJugadores();
-    } else if (opcion == 5){
-        jugador_prueba.masMinutosCancha();
-        cout << "Jugador con más minutos en cancha es: " << jugador_prueba.getNombre() << " con " << jugador_prueba.getMinutosJugados() << " minutos";
-    } else if (opcion == 6){
-        if (equipo.getNombreEquipo() == ""){
-            cout << "Nombre del equipo: ";
-            cin >> nombre_equipo_nuevo;
-            equipo.setNombreEquipo(nombre_equipo_nuevo);
-        } else {
-            cout << "Nuevo nombre del equipo: ";
-            cin >> nombre_equipo_nuevo;
-            equipo.setNombreEquipo(nombre_equipo_nuevo);
+}
+int main() {
+    string nombre_buscar, equipo_buscar, nuevo_nombre_equipo;
+    int opcion1, opcion2, tam, opcion;
+    EquipoNuevo equipo;
+    JugadorNuevo jugador;
+    Partidos partido;
+    cout << "Selecciona tu opción: " << endl;
+    cout << "1. Administrar equipo" << endl;
+    cout << "2. Crear nuevo equipo" << endl;
+    cout << "0. Salir" << endl;
+    cin >> opcion2;
+    while (opcion2 != 0) {
+        if (opcion2 == 1) {
+        cout << "Nombre del equipo: ";
+        cin >> equipo_buscar;
+        EquipoNuevo* equipo_nuevo = partido.buscarEquipo(equipo_buscar);
+        cout << equipo_nuevo->getNombreEquipo() << " prueba " << endl;
+        menu_administrar();
+        cin >> opcion1;
+        if (opcion1 == 1) {
+            cout << "Nombre a buscar: ";
+            cin >> nombre_buscar;
+            partido.getInformacionEquipo().imprimirInformacion(nombre_buscar);
+        } else if (opcion1 == 2) {
+            cout << partido.getInformacionEquipo().mayorEncestador();
+        } else if (opcion1 == 3) {
+            cout << "Nombre a buscar: ";
+            cin >> nombre_buscar;
+            equipo_nuevo->imprimirInformacion(nombre_buscar);
+            cout << "Selecciones lo que le gustaría cambiar: ";
+            cin >> opcion;
+            equipo_nuevo->buscarJugador(nombre_buscar)->cambioInformacion(opcion);
+        } else if (opcion1 == 4) {
+            equipo_nuevo->agregarJugadores();
+            equipo_nuevo->imprimirInformacion("Elian");
+        } else if (opcion1 == 5) {
+            cout << partido.getInformacionEquipo().masMinutosJugados();
+        } else if (opcion1 == 6) {
+            cin >> nuevo_nombre_equipo;
+            partido.getInformacionEquipo().setNombreEquipo(nuevo_nombre_equipo);
+        } else if (opcion1 == 7) {
+            cout << partido.getInformacionEquipo().puntosEquipo();
+        } else if (opcion1 == 8) {
+            cin >> nombre_buscar;
+            partido.getInformacionEquipo().eliminarJugador(nombre_buscar);
         }
-    } else if (opcion == 7){
-        cout << "La cantidad de puntos anotados del equipo " << equipo.calcularPuntos();
-    } else if (opcion == 8){
-        if (equipo.getEntrenador() == ""){
-            cout << "Nombre entrenador: ";
-            cin >> nombre_entrenador;
-            equipo.setNombreEquipo(nombre_entrenador);
-        } 
-    } 
+        } else {
+            cout << "Cantidad de jugadores: ";
+            cin >> tam;
+            for (int i = 0; i < tam; i++) {
+                equipo.agregarJugadores();
+            }
+            cout << "Nombre del nuevo equipo: ";
+            cin >> nuevo_nombre_equipo;
+            equipo.setNombreEquipo(nuevo_nombre_equipo);
+            partido.agregarEquipos(equipo);
+        }
+        cout << "Selecciona tu opción: " << endl;
+        cout << "1. Administrar equipo" << endl;
+        cout << "2. Crear nuevo equipo" << endl;
+        cout << "0. Salir" << endl;
+        cin >> opcion2;
+    }
+    partido.crearHorario();
     return 0;
 }
