4dfe94f9b2d5ccdcd013a4688b6c75aa08fc1ce6
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-12-02 23:14:42 -0600

    Clase hija "Alero"

--- Alero.h
+++ Alero.h
@@ -1,30 +1,53 @@
-#ifndef ALERO_H
-#define ALERO_H
+/*
+- Benjamín Iván Arauz Córdova
+- A00836936
+- 02/12/2022
 
+Clase Alero: Representa a un tipo de jugador, heredado de Jugador, principalmente 
+se caracteriza por tener buenas jugadas.
+*/
+
+#ifndef ALERO_H //Si no esta definido JUGADOR_H
+#define ALERO_H //Definir procesador JUGADOR_H 
+
+//Librerias
 #include <iostream>
 #include <string>
 using namespace std;
 
-#include "JugadorNuevo.h"
+//Clase incluida
+#include "Jugador.h"
 
-class Alero : public JugadorNuevo {
+//Clase heredada de jugador
+class Alero : public Jugador {
     private:
         int buenas_jugadas;
+        Jugador jugador;
     public:
-        Alero(int buenas_jugadas):JugadorNuevo(){
+        //Constructor por omisión
+        Alero(){}
+        //Constructor con variables
+        Alero(int buenas_jugadas, Jugador jugador):Jugador(){
             this->buenas_jugadas = buenas_jugadas;
+            this->jugador = jugador;
         }
+        //Getters
+        Jugador getJugador();
         int getBuenasJugadas();
+        //Setters
         void setBuenasJugadas(int);
 
 };
-
+//Getters
+Jugador Alero::getJugador(){
+    return jugador;
+}
 int Alero::getBuenasJugadas(){
     return buenas_jugadas;
 }
+//Setters
 void Alero::setBuenasJugadas(int nuevo_buenas_jugadas){
     buenas_jugadas = nuevo_buenas_jugadas;
 }
 
 #endif
-
