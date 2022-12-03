949b5398be9ac6ba1a5db2f60ab90f2d8a292353
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-12-02 23:15:01 -0600

    Clase hija "Armador"

--- Armador.h
+++ Armador.h
@@ -1,30 +1,53 @@
-#ifndef ARMADOR_H
-#define ARMADOR_H
+/*
+- Benjamín Iván Arauz Córdova
+- A00836936
+- 02/12/2022
 
+Clase Alero: Representa a un tipo de jugador, heredado de Jugador, principalmente 
+se caracteriza por tener buenas jugadas.
+*/
+
+#ifndef ARMADOR_H //Si no esta definido ARMADOR_H
+#define ARMADOR_H //Definir procesador ARMADOR_H 
+
+//Librerias
 #include <iostream>
 #include <string>
 using namespace std;
 
-#include "JugadorNuevo.h"
+//Clase incluida
+#include "Jugador.h"
 
-class Armador : public JugadorNuevo {
+//Clase heredada de jugador
+class Armador : public Jugador {
     private:
         int robos_hechos;
+        Jugador jugador;
     public:
-        Armador(int robos_hechos):JugadorNuevo(){
+        //Constructor por omisión
+        Armador(){}
+        //Constructor con variables
+        Armador(int robos_hechos, Jugador jugador):Jugador(){
             this->robos_hechos = robos_hechos;
+            this->jugador = jugador;
         }
+        //Getters
+        Jugador getJugador();
         int getRobosHechos();
+        //Setters   
         void setRobosHechos(int);
 
 };
-
+//Getters
+Jugador Armador::getJugador(){
+    return jugador;
+}
 int Armador::getRobosHechos(){
     return robos_hechos;
 }
+//Setters
 void Armador::setRobosHechos(int nuevo_robos){
     robos_hechos = nuevo_robos;
 }
-
 
 #endif
