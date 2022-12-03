d511f1d588a06d478ac6bccc1b28f386e4e08350
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-12-02 23:17:08 -0600

    Clase hija "Poste"

--- Poste.h
+++ Poste.h
@@ -1,28 +1,53 @@
-#ifndef POSTE_H
-#define POSTE_H
+/*
+- Benjamín Iván Arauz Córdova
+- A00836936
+- 02/12/2022
 
+Clase Poste: Representa a un tipo de jugador, heredado de Jugador, principalmente
+se caracteriza por tener rebotes.
+*/
+
+#ifndef POSTE_H //Si no esta definido JUGADOR_H
+#define POSTE_H //Definir procesador JUGADOR_H 
+
+//Librerias
 #include <iostream>
 #include <string>
 using namespace std;
 
-#include "JugadorNuevo.h"
+//Clase incluida
+#include "Jugador.h"
 
-class Poste : public JugadorNuevo {
+//Clase heredada de Jugador
+class Poste : public Jugador {
     private:
+        //Atributos
+        Jugador jugador;
         int rebotes;
     public:
-        Poste(int rebotes):JugadorNuevo(){
+        //Constructor por omisión
+        Poste(): Jugador(){};
+        //Constructor con variables
+        Poste(int rebotes, Jugador jugador):Jugador(){
             this->rebotes = rebotes;
+            this->jugador = jugador;
         }
-        int getBuenasJugadas();
-        void setBuenasJugadas(int);
+        //Getters
+        Jugador getJugador();
+        int getRebotes();
+        //Setters
+        void setRebotes(int);
 
 };
-
-int Poste::getBuenasJugadas(){
+//Getters
+Jugador Poste::getJugador(){
+    return jugador;
+}
+int Poste::getRebotes(){
     return rebotes;
 }
-void Poste::setBuenasJugadas(int nuevo_rebotes){
+//Setters
+void Poste::setRebotes(int nuevo_rebotes){
     rebotes = nuevo_rebotes;
 }
 
