f0077fff4810ced05728ade5d59978702eb02084
BenjaminArauz <111297680+BenjaminArauz@users.noreply.github.com>
2022-12-02 23:16:39 -0600

    Clase "Partidos"

--- Partidos.h
+++ Partidos.h
@@ -1,39 +1,59 @@
-#ifndef PARTIDOS_H
-#define PARTIDOS_H
+/*
+- Benjamín Iván Arauz Córdova
+- A00836936
+- 02/12/2022
+
+Clase Partidos: Representa a la unión de equipos, principalmente para crear al partido.
+*/
+
+#ifndef PARTIDOS_H //Si no esta definido PARTIDO_H 
+#define PARTIDOS_H //Definir procesador PARTIDO_H 
+
+//Librerias
 #include <string>
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
 #include <vector>
-#include "EquipoNuevo.h"
+
+//Clase incluida
+#include "Equipo.h"
 
 class Partidos {
     private:
-        EquipoNuevo informacion_equipo;
-        vector<EquipoNuevo> equipos;
+        //Atributos
+        Equipo informacion_equipo;
+        vector<Equipo> equipos;
     public:
+        //Constructor
         Partidos(){}
-        EquipoNuevo getInformacionEquipo();
-        void agregarEquipos(EquipoNuevo);
-        EquipoNuevo* buscarEquipo(string);
+        //Getter
+        Equipo getInformacionEquipo();
+        //Otros métodos
+        void agregarEquipos(Equipo);
+        Equipo* buscarEquipo(string);
         void crearHorario();
-};
+        void agregarPrimeraDivision(Equipo);
+        void agregarSegundaDivision(Equipo);
 
-EquipoNuevo Partidos::getInformacionEquipo(){
+};
+//Getter
+Equipo Partidos::getInformacionEquipo(){
     return informacion_equipo;
 }
-void Partidos::agregarEquipos(EquipoNuevo equipo){
+//Otros métodos
+void Partidos::agregarEquipos(Equipo equipo){
     equipos.push_back(equipo);
 }
-EquipoNuevo* Partidos::buscarEquipo(string equipo_buscar){
+Equipo* Partidos::buscarEquipo(string equipo_buscar){
     for (int i=0;i<equipos.size();i++){
         if (equipos[i].getNombreEquipo() == equipo_buscar){
             informacion_equipo = equipos[i];
             return &equipos[i];
         }
     }
-    return new EquipoNuevo ();
+    return new Equipo ();
 }
 void Partidos::crearHorario(){
     srand(time(NULL));
